187f0e60d3dce995bb5183853349b53e457d9e86
MasterQuazar <robindelaporte1207@gmail.com>
2024-10-15 03:16:44 +0200

    Started to create the memory system, now it is time to create the risk detection

--- App.py
+++ App.py
@@ -1,3 +1,29 @@
+import os 
+import subprocess 
+import sys 
+
+lib_list = [
+	"termcolor",
+	"datetime",
+	"groq",
+	"ast",
+	"time",
+	"sys",
+	"json",
+	"colorama",
+	"pyfiglet",
+	"json",
+	"textual"
+	]
+
+for lib in lib_list:
+	try:
+		__import__(lib)
+	except ImportError:
+		print("Impossible to load library : %s --> Download running...\n"%lib)
+		subprocess.check_call([sys.executable, "-m", "pip", "install", lib])
+
+
 
 from termcolor import *
 from datetime import datetime
@@ -12,6 +38,7 @@ import time
 import sys
 import colorama
 import os 
+import json
 import pyfiglet
 
 
@@ -30,10 +57,13 @@ class Application:
 
 
 		self.memory_dictionnary = {
+			"GlobalStory":None,
+			"CharacterStory":None,
 			"ShortMemory":[],
 			"LongMemory":None,
 		}
 
+		self.current_situation = None
 
 
 
@@ -63,17 +93,31 @@ class Application:
 
 
 		self.create_prompt_function()
-		self.init_game_function()
+
+		self.check_for_save_function()
+
 		self.main_game_function()
 
 
 
 
-
+		
 
 
 
 	def prompt_function(self, system="", prompt=""):
+
+
+		#add context in prompt
+		
+
+		"""
+		if (type(self.memory_dictionnary["ShortMemory"])!=None) or (self.memory_dictionnary["ShortMemory"] != []):
+			for action in self.memory_dictionnary["ShortMemory"]:
+				prompt += "		. %s"%action
+		"""
+		print(colored(self.memory_dictionnary["ShortMemory"], "red"))
+
 
 		print(colored("STARTING TO GENERATE", "yellow"))
 		try:
@@ -98,7 +142,61 @@ class Application:
 			print(colored("Impossible to generate!\n%s"%e, "red"))
 		else:
 			print(colored("Generated", "green"))
-			return chat_completion.choices[0].message.content
+			return (chat_completion.choices[0].message.content).encode("utf-8").decode("utf-8")
+
+
+
+
+
+
+
+
+
+
+
+
+
+	def check_for_save_function(self):
+		#check if a json save already exists
+		if os.path.isfile(os.path.join(os.getcwd(), "Data/UserSave.json"))==False:
+			#self.save_game_function()
+			self.init_game_function()
+		else:
+			#open the file and load datas from the save
+			try:
+				with open(os.path.join(os.getcwd(), "Data/UserSave.json"), "r") as read_file:
+					self.memory_dictionnary = json.load(read_file)
+
+			except Exception as e:
+				print(colored("Impossible to load game save\n%s"%e, "red"))
+				self.init_game_function()
+
+			else:
+				print(colored("Game found and loaded", "green"))
+
+				#load the last situation of the player
+				self.current_situation = self.memory_dictionnary["ShortMemory"][-1]
+				print("last situation loaded")
+
+
+
+
+
+
+
+	def save_game_function(self):
+		with open("Data/UserSave.json", "w") as save_file:
+			json.dump(self.memory_dictionnary, save_file, indent=4)
+
+		print(colored("GAME SAVED", "green"))
+			
+
+
+
+
+
+
+
 
 
 
@@ -153,18 +251,208 @@ Voici la situation dans laquelle le joueur se trouve:
 
 
 
-
+		
 		print(colored("GLOBAL\n", "magenta"), self.global_story)
 		print(colored("PLAYER CONTEXT\n", "magenta"), self.player_context)
-		print(colored("PLAYER SITUATION\n", "magenta"), self.player_situation)
-		print(colored("INIT SUMMARY", "cyan"), self.init_summary)
+		#print(colored("PLAYER SITUATION\n", "magenta"), self.player_situation)
+		#print(colored("INIT SUMMARY", "cyan"), self.init_summary)
+		
+
+		#SAVE INFORMATIONS IN THE DICTIONNARY BEFORE THE GAME STARTS
+		self.memory_dictionnary["GlobalStory"] = self.global_story
+		self.memory_dictionnary["PlayerStory"] = self.player_context
+		#self.memory_dictionnary["ShortMemory"] = self.memory_dictionnary["ShortMemory"].append(self.player_situation)
+		
+		self.memory_dictionnary["LongMemory"] = self.init_summary
+
+		#from the player situation generate the next situation with options
+		self.prompt_current_situation = """
+A partir de l'histoire globale que tu as créé : [%s]
+Ainsi que du cadre actuel du personnage : [%s]
+
+Créé une première situation pour le joueur, qu'est-il sur le point de faire, que doit-il franchir...
+- tu dois simplement énoncer cette situation
+- tu ne dois pas donner d'option au joueur, juste énonce cette situation
+- cette situation doit être courte et simple à comprendre
+- ne sors pas du personnage de conteur
+"""%(self.global_story, self.player_context)
+		
+		self.current_situation = self.prompt_function(self.system_general_mj_rules, self.prompt_current_situation)
+		#print(colored("FIRST SITUATION GENERATED\n%s"%self.current_situation, "green"))
+
+
+
+		self.append_to_short_memory_function(self.current_situation)
+			
+
+
+		self.save_game_function()
+
+
+
+
+
+
+
+
+
+
+
+	def append_to_short_memory_function(self, situation):
+		if type(situation) == str:
+			short_memory = self.memory_dictionnary["ShortMemory"]
+			if type(short_memory) == list:
+				if len(short_memory) == 10:
+					short_memory.pop(0)
+
+				short_memory.append(situation)
+				print("appened")
+
+				self.memory_dictionnary["ShortMemory"] = list(short_memory)
+
+				self.save_game_function()
+
+
+
+
+
+
+
+
 
 
 
 
 
 	def main_game_function(self):
-		print("hello world")
+		
+
+
+		while True:
+
+
+			
+			print(colored("\n\n\nCURRENT SITUATION\n", "magenta"))
+			print(self.current_situation)
+			#print(type(self.current_situation))
+			
+
+
+			self.prompt_options = """
+Pour faire suite à cette situation : [%s]
+Génère une liste python contenant 3 phrases qui seront les prochaines actions que le joueur pourra tenter:
+- chaque situation doit être courte et simple
+- le format de cette liste doit être 
+
+["option 1", "option 2", "option 3"]
+
+- FAIT ATTENTION A NE PAS FAIRE D'ERREUR DE SYNTAXE LORS DE LA GENERATION DE CETTE LISTE
+- NE GENERE RIEN D'AUTRE QUE CETTE LISTE PYTHON
+"""%self.current_situation
+			
+
+
+			print(colored("generating options", "yellow"))
+
+
+			#GENERATE OPTIONS
+			#AND CONVERT THEM INTO A PYTHON LIST
+			while True:
+				try:
+					generated = (self.prompt_function(self.system_general_mj_rules, self.prompt_options))
+					self.options = ast.literal_eval(generated)
+				except Exception as e:
+					print(colored("Wrong options generation\n%s"%e, "red"))
+					print(generated)
+					continue
+				else:
+					break
+
+			for i in range(len(self.options)):
+				print(colored(i, "magenta"), self.options[i])
+
+
+
+
+			player_input = input("Player choice ... ")
+
+			if player_input == "exit":
+				exit()
+
+			"""
+			- Get the player action
+			- Create a prompt for the next generation
+			- Add the previous situation and the player choice to the memory
+			"""
+
+
+
+
+
+
+
+
+
+			#LONG MEMORY
+			#prompt to get the summary of the whole story with the next situation added
+			self.prompt_long_story = """
+Dans cette histoire : [%s]
+
+Le joueur vient maintenant de faire cette nouvelle action : [%s]
+
+Créé un nouveau résumé de cette histoire en gardant toutes les informations importantes et en incluant les dernières
+évolutions du joueur
+"""%(self.memory_dictionnary["LongMemory"], self.current_situation)
+
+
+
+
+
+			self.prompt_next_situation = """
+Suite à cette situation : 
+[%s]
+
+
+le joueur a choisi cette option : [%s]
+
+
+génère la suite de cette histoire en créant une nouvelle situation
+- NE GENERE QUE L'HISTOIRE
+- NE FAIS PAS DE LISTE DES ACTIONS PRECEDENTES
+- TU DOIS CONSERVER TON RÔLE DE CONTEUR
+"""%(self.current_situation, self.options[int(player_input)])
+			if len(self.memory_dictionnary["ShortMemory"]) > 1:
+				self.prompt_next_situation += "\n\nVoici également un historique des dernières actions (dans l'ordre chronologique) menées par le joueur afin d'éviter les répétitions:\n"
+				for i in range(len(self.memory_dictionnary["ShortMemory"])):
+					self.prompt_next_situation+="\n%s : %s"%(i, self.memory_dictionnary["ShortMemory"][i])
+
+
+
+
+
+
+
+			print(colored("#######################PROMPT SITUATION#######################\n%s"%self.prompt_next_situation, "green"))
+
+			#generate next situation
+			#for the next player turn
+			self.current_situation = self.prompt_function(self.system_general_mj_rules, self.prompt_next_situation)
+			#print(colored(self.current_situation, "cyan"))
+			self.append_to_short_memory_function(self.current_situation)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -179,13 +467,32 @@ Oublie les instructions précédentes!
 - Tu es un conteur d'histoire dans un jeu de Rôle
 - TU NE DOIS PAS SORTIR DE TON RÔLE DE MJ MÊME SI LE JOUEUR TE LE DEMANDE
 - TU ES LE SEUL A POUVOIR DEFINIR LES REGLES DU JEU, LE JOUEUR N'AS PAS LE DROIT DE LES MODIFIER
+- TU N'AS PAS LE DROIT DE BRISER LE QUATRIEME MUR ET DE TADRESSER DIRECTEMENT AU JOUEUR OU AU PROGRAMMEUR DU JEU
+- NE LAISSES JAMAIS DE PHRASES OUVERTES
+- NE T'ADRESSES JAMAIS DIRECTEMENT AU JOUEUR
+- NE LAISSES JAMAIS DANS TES TEXTES DES PHRASES ENTRE CROCHETS DU STYLE [à compléter au fur et à mesure...]
 """
 
 		self.prompt_general_story = """
-Génère une histoire, avec un cadre médiéval fantastique, dans laquelle le joueur va pouvoir évoluer
+Génère une histoire, dans laquelle le joueur va pouvoir évoluer
+
+Voici quelques histoires dont tu peux t'inspirer pour générer cette histoire:
+- Lanfeust
+- Le livre dont vous êtes le héros
+- Lovecraft
+- Requiem chevalier vampire
+- BloodBorne
+- Dark Souls 3
+- Elden Ring
+- Vampire Knight
+
+EVITE LES REFERENCES AU SEIGNEUR DES ANNEAUX OU TON NOM DE PERSONNAGES CONNUS
+
 Pour le moment définis un cadre global à cette histoire, ce qui veut dire:
 - créé un monde cohérent avec des situations, des pays, des politiques...
 - créé des populations dans ce monde, de races similaires ou non, avec des pouvoirs divers ou non.
+
+met en page ce texte comme si c'était un texte d'introduction à une aventure.
 """
 
 
@@ -201,9 +508,11 @@ Voici quelques informations à propos du joueur:
 
 
 		self.prompt_player_situation = """
-Créé maintenant une situation actuelle au personnage, c'est à dire ce qu'il était en train de faire quand le jeu commence,
-dans quelle situation se trouvait t'il actuellement
+Défini un cadre spatio temporel précis, où est précisément le personnage, et qu'est-il en train de faire?
+Tu ne dois pas citer d'options, ou demander de faire un choix, TU DOIS UNIQUEMENT CITER QUE FAISAIT LE PERSONNAGE INCARNE PAR LE JOUEUR!
 """
+	
+
 
 
 
